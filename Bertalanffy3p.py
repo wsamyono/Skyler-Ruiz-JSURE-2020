34d204dc52f453924c1107626f3521128da7eb8f
SkylerJulianne <67381865+SkylerJulianne@users.noreply.github.com>
2020-07-05 21:05:31 -0500

    Add files via upload

---
+++ Bertalanffy3p.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 09:51:39 2020
+
+@author: skyler
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.optimize import minimize
+# from scipy.interpolate import interp1d
+
+# Import CSV data file
+# Column 1 = time (t)
+# Column 2 = input (u)
+# Column 3 = output (yp)
+data = np.loadtxt('data1.txt',delimiter=',')
+# u0 = data[0,1]
+# yp0 = data[0,1]
+# t = data[:,0].T - data[0,0]
+t = data[:,0]
+# u = data[:,1].T
+yp = data[:,1]
+
+# specify number of steps
+ns = len(t)
+# delta_t = t[1]-t[0]
+# create linear interpolation of the u data versus time
+# uf = interp1d(t,u)
+
+# define first-order plus dead-time approximation    
+# def fopdt(y,t,uf,Km,taum,thetam)
+def fopdt(y,t,x,alpha1, alpha2, beta1):
+    # arguments
+    #  y      = output
+    #  t      = time
+    #  uf     = input linear function (for time shift)
+    #  Km     = model gain
+    #  taum   = model time constant
+    #  thetam = model time constant
+    # time-shift u
+    # logistic = dydt = (alpha1*y)/(y + beta1)
+  #  try:
+    beta1 = x[2]
+    if t <= 4:
+                alpha1 = x[0]
+                dydt = alpha1 * (y ** 2/3) - beta1 * y
+    else:
+                alpha2 = x[1]
+                dydt = alpha2 * (y ** 2/3) - beta1 * y
+  #  except:
+    #    print('Error with time extrapolation: ' + str(t))
+    #    um = 0
+    # calculate derivative
+    #    dydt = Km*y                # (-(y-yp0))*Km # + Km * (um-u0))/taum
+    return dydt
+
+# simulate FOPDT model with x=[Km,taum,thetam]
+def sim_model(x):
+    # input arguments
+    alpha1 = x[0]
+    alpha2 = x[1]
+    # taum = x[1]
+    beta1 = x[2]
+    # storage for model values
+    ym = np.zeros(ns)  # model
+    # initial condition
+    ym[0] = 50000
+    # loop through time steps    
+    for i in range(0,ns-1):
+        ts = [t[i],t[i+1]]
+  #              y1 = odeint(fopdt,ym[i],ts,args=(uf,Km,taum,thetam))
+        y1 = odeint(fopdt,ym[i],ts,args=(x,alpha1,alpha2, beta1))
+        ym[i+1] = y1[-1]
+    return ym
+
+# define objective
+def objective(x):
+    # simulate model
+    ym = sim_model(x)
+    # calculate objective
+    obj = 0.0
+    for i in range(len(ym)):
+        obj = obj + (ym[i]-yp[i])**2    
+    # return result
+    return obj
+
+# initial guesses
+x0 = np.zeros(3)
+x0[0] = 100000 # aplpha1
+x0[1] = -200 # alpha2
+x0[2] = 200# beta
+
+# show initial objective
+print('Initial SSE Objective: ' + str(objective(x0)))
+print('alpha01: ' + str(x0[0]),'alpha02: ' + str(x0[1]), 'and beta01: ' + str(x0[2]))
+# optimize Km, taum, thetam
+solution = minimize(objective,x0)
+
+# Another way to solve: with bounds on variables
+#bnds = ((0.4, 0.6), (1.0, 10.0), (0.0, 30.0))
+#solution = minimize(objective,x0,bounds=bnds,method='SLSQP')
+x = solution.x
+
+# show final objective
+print('Final SSE Objective: ' + str(objective(x)))
+
+print('alpha1: ' + str(x[0]),'alpha2: ' + str(x[1]), 'and beta1: ' + str(x[2]))
+# print('taup: ' + str(x[1]))
+# print('thetap: ' + str(x[2]))
+
+# calculate model with updated parameters:
+ym1 = sim_model(x0)
+ym2 = sim_model(x)
+# plot results
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(t,yp,'kx-',linewidth=2,label='Data')
+plt.plot(t,ym1,'b-',linewidth=2,label='Initial Guess')
+plt.plot(t,ym2,'r--',linewidth=3,label='Optimized Model')
+plt.ylabel('Output')
+plt.legend(loc='best')
+#plt.subplot(2,1,2)
+#plt.plot(t,x[0],'bx-',linewidth=2)
+#plt.plot(t,x[1],'r--',linewidth=3)
+# plt.legend(['Measured','Interpolated'],loc='best')
+# plt.ylabel('Input Data')
+plt.show()
